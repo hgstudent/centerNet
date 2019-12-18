7e984e062a5e54f45ec76d70ca035643240cd883
Hannes Gustafsson <ghannes@student.chalmers.se>
2019-12-18 19:04:09 +0100

    Create loss.py
    push and pull loss

---
+++ loss.py
@@ -0,0 +1,22 @@
+def push_pull(pred_tl, pred_br, mask_tl, mask_br):
+  s1 = tf.gather_nd(pred_tl, mask_tl)
+  s2 = tf.gather_nd(pred_br, mask_br)
+  push, pull = 0, 0
+  m = []
+  
+  for a,b in zip(s1,s2):
+    mean = (a+b)/2
+    pull += tf.pow(a-mean, 2) + tf.pow(b-mean,2)
+    m.append(mean)
+
+  m = tf.convert_to_tensor(m)
+  for i in range(len(m)):
+    mask = np.ones(len(m))
+    mask[i] = 0
+    mask = tf.convert_to_tensor(mask)
+
+    p = 1 - tf.math.abs(m - tf.gather_nd(m, [i]))
+    p = p*mask
+    push += tf.reduce_sum(p)
+    
+  return push, pull
