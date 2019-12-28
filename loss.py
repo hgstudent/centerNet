964f55e703a811692c57d7418d0582331dd77094
Hannes Gustafsson <ghannes@student.chalmers.se>
2019-12-28 14:13:07 +0100

    Update loss.py

--- loss.py
+++ loss.py
@@ -27,3 +27,12 @@ def push_pull(pred_tl, pred_br, mask_tl, mask_br):
   push = push/(N*(N-1))  
 
   return push, pull
+
+def extract_masks(gt, dtype_="int32"):
+  gt = tf.cast(gt, dtype=dtype_)
+  cond = tf.where(tf.math.not_equal(gt, 0))
+  c = cond[::2, 0]
+  c = tf.reshape(c, (-1, 1))
+  c = tf.cast(c, dtype=dtype_)
+  c = tf.concat([c,tf.reshape(tf.gather_nd(gt, cond), (2,-1))], axis=-1)
+  return c
