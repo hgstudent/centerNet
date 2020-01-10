931a53e867d7fc95fd3db1c82815c1f4c71f98ae
Hannes Gustafsson <ghannes@student.chalmers.se>
2020-01-10 17:08:52 +0100

    Update model.py

--- model.py
+++ model.py
@@ -3,22 +3,24 @@ from unet import *
 import tensorflow as tf
 
 def model(c = 1, input_size=(512,512,3)):
-  inputs = tf.keras.Input(shape=input_size)
-  x = conv_module(inputs, size=(7,7), strides_=(2,2))
-  x = conv_module(x, size=(3,3), strides_=(2,2))
-
-  x = unet(x)
+  #Decrease resolution and extract feature map
+  inputs      = tf.keras.Input(shape=input_size)
+  x           = conv_module(inputs, size=(7,7), strides_=(2,2))
+  x           = conv_module(x, size=(3,3), strides_=(2,2))
+  x           = unet(x)
   feature_map = unet(x)
+  
+  
   #Top left
-  tl = tl_pooling(feature_map)
-  tl = tf.keras.layers.Conv2D(256, (3,3), padding="same")(tl)
-  tl = tf.keras.layers.BatchNormalization()(tl)
+  tl    = tl_pooling(feature_map)
+  tl    = tf.keras.layers.Conv2D(256, (3,3), padding="same")(tl)
+  tl    = tf.keras.layers.BatchNormalization()(tl)
 
-  x = tf.keras.layers.Conv2D(256, (1,1), padding="same")(feature_map)
+  x     = tf.keras.layers.Conv2D(256, (1,1), padding="same")(feature_map)
 
-  x = tl + x
-  x = tf.keras.layers.ReLU()(x)
-  x = conv_module(x)
+  x     =  tl + x
+  x     = tf.keras.layers.ReLU()(x)
+  x     = conv_module(x)
 
   #Top left heatmap
   tl_hm = tf.keras.layers.Conv2D(256, (3,3), activation="relu", padding="same")(x)
@@ -31,15 +33,15 @@ def model(c = 1, input_size=(512,512,3)):
 
 
   #Bottom right
-  br = br_pooling(feature_map)
-  br = tf.keras.layers.Conv2D(256, (3,3), padding="same")(br)
-  br = tf.keras.layers.BatchNormalization()(br)
+  br    = br_pooling(feature_map)
+  br    = tf.keras.layers.Conv2D(256, (3,3), padding="same")(br)
+  br    = tf.keras.layers.BatchNormalization()(br)
 
-  x = tf.keras.layers.Conv2D(256, (1,1), padding="same")(feature_map)
+  x     = tf.keras.layers.Conv2D(256, (1,1), padding="same")(feature_map)
 
-  x = br + x
-  x = tf.keras.layers.ReLU()(x)
-  x = conv_module(x)
+  x     = br + x
+  x     = tf.keras.layers.ReLU()(x)
+  x     = conv_module(x)
 
   #Bottom right heatmap
   br_hm = tf.keras.layers.Conv2D(256, (3,3), activation="relu", padding="same")(x)
