3d8896eeac3d0e0303033e2415366013f327a230
Hannes Gustafsson <ghannes@student.chalmers.se>
2020-01-13 12:52:04 +0100

    Update train.py

--- train.py
+++ train.py
@@ -5,16 +5,17 @@ from utils import *
 from model import *
 from loss import *
 
-voc_train = tfds.load(name="voc", split="train")
 
+voc_train = tfds.load(name="voc", split="train")
 optimizer = tf.keras.optimizers.Adam(learning_rate=2.5e-4)
 shapes    = ([None, None, 3], [None, 4])
 p_values  = (tf.constant(0, dtype=tf.float32), tf.constant(-1, dtype=tf.int32))
-epochs = 100
+epochs    = 100
 
 for epoch in range(epochs):
   loss_comp = 0
   i         = 0
+  
   for voc_example in voc_train.map(preprocess_img).padded_batch(5,  padded_shapes=shapes, padding_values=p_values): 
     
     image, label  = voc_example[0], voc_example[1]
@@ -23,6 +24,7 @@ for epoch in range(epochs):
     with tf.GradientTape() as tape:  
       logits  = m(image)
       loss_   = loss_fn(logits, [tl, br])
+      
     loss_cmp += tf.reduce_sum(loss_)
     grads = tape.gradient(loss_, m.trainable_weights)
     optimizer.apply_gradients(zip(grads, m.trainable_weights))
