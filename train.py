0020a3d82670788ccec5d07c54c6cf080cfd131a
Hannes Gustafsson <ghannes@student.chalmers.se>
2020-01-22 15:51:25 +0100

    Update train.py

--- train.py
+++ train.py
@@ -6,18 +6,19 @@ from model import *
 from loss import *
 
 
-voc_train = tfds.load(name="voc", split="train")
-optimizer = tf.keras.optimizers.Adam(learning_rate=2.5e-4)
-shapes    = ([None, None, 3], [None, 4])
-p_values  = (tf.constant(0, dtype=tf.float32), tf.constant(-1, dtype=tf.int32))
-epochs    = 100
-m         = centerNet()
+voc_train   = tfds.load(name="voc", split="train")
+optimizer   = tf.keras.optimizers.Adam(learning_rate=2.5e-4)
+shapes      = ([None, None, 3], [None, 4])
+p_values    = (tf.constant(0, dtype=tf.float32), tf.constant(-1, dtype=tf.int32))
+epochs      = 100
+batch_size  = 5
+m           = centerNet()
 
 for epoch in range(epochs):
   loss_cmp  = 0
   i         = 0
   
-  for voc_example in voc_train.map(preprocess_img).padded_batch(5,  padded_shapes=shapes, padding_values=p_values): 
+  for voc_example in voc_train.map(preprocess_img).padded_batch(batch_size,  padded_shapes=shapes, padding_values=p_values): 
     
     image, label  = voc_example[0], voc_example[1]
     tl, br        = extract_masks(label[:,:,0:2]), extract_masks(label[:,:,2:4])
@@ -32,5 +33,5 @@ for epoch in range(epochs):
     i = i + 1
   
   print("Epoch: "       + str(epoch))
-  print("Total loss: "  + str(loss_cmp/i))
+  print("Total loss: "  + str(loss_cmp/(batch_size*i))
   print()
